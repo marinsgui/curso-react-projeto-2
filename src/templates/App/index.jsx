c392a5d68467a3e40e251d72987b3178e589faa7
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-09 11:56:12 -0300

    organização utilizando hooks

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { PostsProvider } from '../../contexts/PostsProvider';
+import { Posts } from '../../components/Posts';
+
+function App() {
+  return (
+    <PostsProvider>
+      <div className='App'>
+        <Posts />
+      </div>
+    </PostsProvider>
+  )
+}
+
+export default App;
